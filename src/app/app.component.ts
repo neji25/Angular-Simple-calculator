0e60eeacb97149db2d75fe015c7d3cdab2091413
Borodiuk Vlad <myborvlad@gmail.com>
2023-04-11 14:30:34 +0300

    Simple calculator

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  numberInput = ''
+  isDark = true
+  isActive = true
+
+  onClear() {
+    this.numberInput = ''
+  }
+
+  addNumberOrOperator(num: string) {
+    const operators = ['*', '/', '+', '-']
+    if(operators.includes(num)) {
+      if(this.numberInput === '' ||
+         this.numberInput[this.numberInput.length-1].includes(num))
+        return
+    }
+    this.numberInput += num
+  }
+
+  onBack() {
+    this.numberInput = this.numberInput.substring(0, this.numberInput.length - 1)
+  }
+
+  onEqual() {
+    if(this.numberInput == '') {
+      this.numberInput = 'Empty!'
+      setTimeout(() => this.numberInput = '', 500)
+      return
+    }
+    this.numberInput = this.parser(this.numberInput)
+  }
+
+  parser(str: string): string {
+    return new Function(`return ${str}`)().toString()
+  }
+
+  toggleTheme() {
+    this.isDark = !this.isDark
+    this.isActive = !this.isActive
+  }
+}
